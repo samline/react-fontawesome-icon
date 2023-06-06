3d59eb1c0e026ff21f60d45a422e02324fcab6f1
Samuel Olvera <jsamline@gmail.com>
2023-06-06 00:15:28 -0600

    added component from react-bee

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,33 @@
-/*  */
+/* Packages */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-export const Package = () => {
-  return (
-    <>
-    <h1>My NPM Package</h1>
-    </>
-  )
+/* Libraries */
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+/* Settings */
+library.add(fab, far, fas)
+
+/* Styles */
+import { Icon } from './styles'
+
+/**
+ * FontAwesomeIcon
+ * @param {string} icon (required) - icon to use. (e.g. 'chevron-right')
+ * @param {string} lib - library to use. - (e.g. fas, far, fab) - default: fas
+ * @param {reference} innerRef - you can still assign to ref
+ * @param {...styledSystemProps} [color, layout, position, space, typography]
+ * @param {...any} props
+ *
+ * @returns component
+ *
+ * @fontawesome docs - https://fontawesome.com/search
+ * @styleSystem docs - https://styled-system.com/table
+ */
+
+export const FontAwesomeIcon = ({ icon, lib = 'fas', innerRef, ...props }) => {
+  return <Icon icon={[lib, icon]} {...props} ref={innerRef} />
 }
+
+export { library }
---
+++ src/styles.js
@@ -0,0 +1,20 @@
+/* Packages */
+import styled from 'styled-components'
+import {
+  color,
+  compose,
+  layout,
+  position,
+  space,
+  typography
+} from 'styled-system'
+
+/* Package Components */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export const Icon = styled(FontAwesomeIcon)`
+  height: 1em;
+  ${compose(color, layout, position, space, typography)}
+`
+
+export default FontAwesomeGlobalStyle
